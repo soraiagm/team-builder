5db60c357f80b648855b32dc0f6f9f690a0a77f3
Soraia <soraia@Soraias-iMac.local>
2019-10-17 16:41:27 -0400

    mvp completed

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
+import TeamList from './components/TeamList';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
-        
+        <TeamList  />
       </header>
     </div>
   );
